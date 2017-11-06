02f983c9173cb4f701ecf89ed7afcc27c19f6ba7
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2017-11-06 23:42:17 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -18,15 +18,21 @@ function transform(){
 				if(text_templateParts[i] == "}}" || text_templateParts[i].indexOf("Haut") != -1 || text_templateParts[i].indexOf("génération") != -1) continue;
 					else {
 						var moveName = text_templateParts[i].remove(/.{1,}\|\s/);
-						var parents_natural = text_templateParts[i+1]?text_templateParts[i+1].remove("| ").remove(/\{\{miniature\|/g).split("}}").customFilter("").customFilter(" "):false;
-						var parents_chain = text_templateParts[i+2]?text_templateParts[i+2].remove("| ").remove(/\{\{miniature\|/g).split("}}").customFilter("").customFilter(" "):undefined;
+						var parents_natural = text_templateParts[i+1]?text_templateParts[i+1].remove("| ").remove(/\{\{miniature\|/g).split("}}"):false;
+						var parents_chain = text_templateParts[i+2]?text_templateParts[i+2].remove("| ").remove(/\{\{miniature\|/g).split("}}"):undefined;
 						if(parents_natural){
+							while(parents_natural.indexOf("") != -1){
+								parents_natural.splice(parents_natural.indexOf(""),1);
+							}
 							var pokéNames_natural = parents_natural.map(function(num){
 								return megaArray[num-1];
 							});
 						}
 						else continue;
 						if(parents_chain){
+							while(parents_chain.indexOf("") != -1){
+								parents_chain.splice(parents_chain.indexOf(""),1);
+							}
 							var pokéNames_chain = parents_chain.map(function(num){
 								return megaArray[num-1];
 							});
@@ -83,10 +89,9 @@ function transform(){
 						mapSpot = mapSpot.remove("| ");
 						if(cost.indexOf("pco") != -1){
 							cost = Number(cost.match(/[0-9]+/));
-							
 						}
 						moveName = moveName.remove("| ").remove("[[").remove(/.{1,}\|/).remove("]]");
-						finalWikitext += moveName + (mapSpot[0] == " "?"/"+mapSpot:"/ "+mapSpot)+" / "+cost+"\n";
+						finalWikitext += moveName + (mapSpot[0] == " "?"/"+mapSpot:"/ "+mapSpot)+(cost?" / "+cost:"")+"\n";
 					}
 				}
 				finalWikitext += "}}";
